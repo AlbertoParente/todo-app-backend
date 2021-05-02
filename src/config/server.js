4dc24df83d3a9bc47cd59b94b92ab905c4f5e9cc
AlbertoParente <albertoparente20@gmail.com>
2021-05-01 22:12:14 -0300

    Config server with express

--- src/config/server.js
+++ src/config/server.js
@@ -1,1 +1,11 @@
 const port = 3003
+
+const bodyParser = require('body-parser')
+const express = require('express')
+
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+
+server.listen(port, function() {
+    console.log(`Backend is running on port ${port}.`)
+})
